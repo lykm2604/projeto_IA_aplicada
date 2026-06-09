00d22fbadc52bed42df8001ba83d2b6d050aeae2
Lucas <lykm2604@gmail.com>
2026-06-09 13:48:37 -0300

    Leitura do arquivo de texto

--- Desafio-Final.py
+++ Desafio-Final.py
@@ -1,7 +1,17 @@
+import json
+from contato_com_LLM import retorno_em_json
 # Etapa 1
+
 lista_de_resenhas = []
 with open("Resenhas_App_ChatGPT.txt", "r", encoding="utf-8") as arquivo:
     for linha in arquivo:
         lista_de_resenhas.append(linha.strip())
 
-print("\n".join(lista_de_resenhas))
+# Etapa 2
+
+lista_de_resenhas_json = []
+
+for resenha in lista_de_resenhas:
+    resenha_json = retorno_em_json(resenha)
+    resenha_dict = (resenha_json)
+    lista_de_resenhas_json.append(resenha_dict)
